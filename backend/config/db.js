d792346097debdd6f36a09557b1a16a15fb85f1e
Harsh <harshj200230@gmail.com>
2024-06-12 19:43:21 +0530

    atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mini-crm', {
+    await mongoose.connect('mongodb+srv://harshj200230:harsh2002@cluster0.68nl8ln.mongodb.net/mini-crm?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log('MongoDB connected to Atlas');
   } catch (error) {
-    console.error(error.message);
+    console.error('Error connecting to MongoDB:', error.message);
     process.exit(1);
   }
 };
